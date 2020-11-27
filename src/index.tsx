7851b4b7db267a282cae4558e6ddb6d7ac44daf7
Victoria Rodriguez <vrodriguezfe@icloud.com>
2020-11-27 19:05:45 -0300

    Feat: Implement routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// ReactDOM.render(<App/>, document.getElementById('root'));
+
+const Routing = () => {
+	return (
+		<Router>
+			<Route exact path="/" component={ App }/>
+		</Router>
+	);
+};
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Routing/>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
