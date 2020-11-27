d50283996e8cbd643aaa2255568a49087ba98491
Victoria Rodriguez <vrodriguezfe@icloud.com>
2020-11-27 18:36:55 -0300

    Test: Create test for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-	return(<h1>Welcome <span role="img" aria-label="pink heart with yellow sparkles">💖</span></h1>);
+	return(
+		<div id="App">
+			<h1>Welcome <span role="img" aria-label="pink heart with yellow sparkles">💖</span></h1>
+		</div>
+	);
 };
 
 export default App;
