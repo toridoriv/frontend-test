912a9b18422f73ab8fbc8a6c480470dabdf49796
Victoria Rodriguez <vrodriguezfe@icloud.com>
2020-12-02 01:47:15 -0300

    Feature: Files with globally used constants
    Because:
    - Import constantly the same files seems redundant. To avoid that, I created singles files to export dependencies and regular constants so then I just have to import one file for dependencies, components, etc.

---
+++ src/constants/_components.ts
@@ -0,0 +1,37 @@
+/* 📌 Atoms */
+export { default as Button } from './../components/atoms/Button';
+export { default as Circle } from './../components/atoms/Circle';
+export { default as Column } from './../components/atoms/Column';
+export { default as Columns } from './../components/atoms/Columns';
+export { default as Hero } from './../components/atoms/Hero';
+export { default as Icon } from './../components/atoms/Icon';
+export { default as Input } from './../components/atoms/Input';
+export { default as Logo } from './../components/atoms/Logo';
+export { default as Tag } from './../components/atoms/Tag';
+
+/* 📌 Molecules */
+export { default as Add } from './../components/molecules/Add';
+export { default as Counter } from './../components/molecules/Counter';
+export { default as Delete } from './../components/molecules/Delete';
+export { default as Dismiss } from './../components/molecules/Dismiss';
+export { default as Loader } from './../components/molecules/Loader';
+export { default as Redirect } from './../components/molecules/Redirect';
+export { default as Retry } from './../components/molecules/Retry';
+export { default as Searchbar } from './../components/molecules/Searchbar';
+export { default as Share } from './../components/molecules/Share';
+export { default as Status } from './../components/molecules/Status';
+
+/* 📌 Organisms */
+export { default as Modal } from './../components/organisms/Modal';
+export { default as Popup } from './../components/organisms/Popup';
+
+/* 📌 Pages */
+export { default as CreateItemScreen } from './../components/pages/CreateItemScreen';
+export { default as MainScreen } from './../components/pages/MainScreen';
+export { default as WelcomeScreen } from './../components/pages/WelcomeScreen';
+
+/* 📌 Templates */
+export { default as FooterLayout } from './../components/templates/FooterLayout';
+export { default as MainLayout } from './../components/templates/MainLayout';
+export { default as ModalLayout } from './../components/templates/ModalLayout';
+export { default as PopupLayout } from './../components/molecules/PopupLayout';
---
+++ src/constants/_variables.ts
@@ -0,0 +1,5 @@
+import { React, ReactDOM } from './_dependencies';
+
+export * as serviceWorker from '../serviceWorker';
+export const el = React.createElement;
+export const domRender = ReactDOM.render;
